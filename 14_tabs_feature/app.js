10ec4368c4f715b99847754f46b8d46080630b74
MANISH KUMAR <152767073+mkumar2003@users.noreply.github.com>
2024-04-09 00:02:14 +0530

    Update app.js

--- 14_tabs_feature/app.js
+++ 14_tabs_feature/app.js
@@ -1,19 +1,30 @@
 const about = document.querySelector(".about");
 const btns = document.querySelectorAll(".tab-btn");
 const articles = document.querySelectorAll(".content");
+
+
+//kahi pe bhi click karo par jab bhi button click hoga tabhi target.dataset.id mai value aayegi and if execute hoga
+//click wherever you want but it will respond only when button is clicked and target.dataset.id returns some value
+
 about.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   if (id) {
-    // remove selected from other buttons
+
+    // remove selected from every buttons
     btns.forEach(function (btn) {
       btn.classList.remove("active");
     });
+    //adding active class to clicked button
     e.target.classList.add("active");
-    // hide other articles
+
+
+    // hide all other articles
     articles.forEach(function (article) {
       article.classList.remove("active");
     });
+    
     const element = document.getElementById(id);
     element.classList.add("active");
   }
 });
+
